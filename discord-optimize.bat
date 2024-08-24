ba059d973f259df9144190bc4ad58727091cb6e7
Tucnicek14 <160906567+Tucnicek14@users.noreply.github.com>
2024-08-24 17:40:40 +0200

    Add files via upload

--- discord-optimize.bat
+++ discord-optimize.bat
@@ -10,20 +10,17 @@ set /p choice=Type 1 or 2 to continue.
 if not '%choice%'=='' set choice=%choice:~0,1%
 if '%choice%'=='2' echo Closing...
 if '%choice%'=='2' C:\Windows\System32\TASKKILL.exe /f /im OpenConsole.exe > nul 2> nul
-if '%choice%'=='1' echo Continuing...
+if '%choice%'=='1' echo Continuing... (ignore any "cannot find the file" errors)
 
 C:\Windows\System32\TASKKILL.exe /f /im Discord.exe > nul 2> nul
 C:\Windows\System32\TASKKILL.exe /f /im Discord.exe > nul 2> nul
 C:\Windows\System32\TASKKILL.exe /f /im Discord.exe > nul 2> nul
 
 cd %localappdata%\Discord\app-1.0.9159\modules\
-if exist discord_cloudsync-1
 rmdir /s discord_cloudsync-1 /q
-if exist discord_dispatch-2
-rmdir /s discord_dispatch-2 /q
-if exist discord_erlpack-1
+rmdir /s discord_dispatch-1 /q
 rmdir /s discord_erlpack-1 /q
-if exist discord_game_utils-1
 rmdir /s discord_game_utils-1 /q
-if exist discord_spellcheck-2
-rmdir /s discord_spellcheck-2 /q
+rmdir /s discord_spellcheck-1 /q
+echo Discord is not debloated! Feel free to close this command prompt now.
+pause
